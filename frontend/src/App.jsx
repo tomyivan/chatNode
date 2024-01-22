21eff234796a978cc9b316103dc75b19850a553b
tomyivan <tomiivan3@gmail.com>
2024-01-22 09:45:47 -0400

    Se creo el servidor para el chat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,46 @@
 import io from "socket.io-client"
+import {useState, useEffect} from "react";
 const socket = io("/")
 function App(){
+  const [message,setMessage] = useState("");
+  const [messages,setMessages] = useState([]);
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    const newMessage = {
+      body: message,
+      from: 'Me'
+    }
+    setMessages([...messages,newMessage]);
+    socket.emit('message',message)
+  };
+  useEffect(()=>{
+    socket.on('message', receiveMessage );
+    return ()=>{
+      socket.off("message",receiveMessage);
+    }
+  },[])
+  const receiveMessage = (message)=>
+    setMessages((state)=>[...state,message])
   return (
-    <div>hello world</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Escribe aqui su mensaje..." 
+          onChange={(e)=>setMessage(e.target.value)}  
+        />
+        <button type="submit">Enviar</button>
+      </form>
+      <ul>
+        {
+          messages.map((message,i)=>(
+            <li key={i}>
+              {message.from}:{message.body}
+              </li>
+          ))
+        }
+      </ul>
+    </div>
   )
 }
 export default App
